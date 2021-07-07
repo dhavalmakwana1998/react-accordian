8045b32730adb4aaf53bc56960a45e6b1b1b377f
Dhaval Makawana <dhavalmakawana191@gmail.com>
2021-07-08 00:42:53 +0530

    changes

--- src/Accordian.jsx
+++ src/Accordian.jsx
@@ -1,36 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Typography, Container } from "@material-ui/core";
-import MyAccordian from './MyAccordian'
-import questions from './api'
-import { makeStyles } from '@material-ui/core/styles';
+import MyAccordian from "./MyAccordian";
+import questions from "./api";
+import { makeStyles } from "@material-ui/core/styles";
 
-
-const useStyles = makeStyles(theme => ({
-    myRoot: {
-        padding: "20px",
-    },
+const useStyles = makeStyles((theme) => ({
+  myRoot: {
+    padding: "20px",
+  },
 }));
 
 function Accordian() {
+  const [question, setQuestion] = useState(questions);
 
-    const [question, setQuestion] = useState(questions);
+  const classes = useStyles();
 
-    const classes = useStyles();
+  return (
+    <>
+      <Container className={classes.myRoot}>
+        <Typography variant="h3" style={{ color: "green" }}>
+          My Custom Accordian Style
+        </Typography>
 
-
-    return (
-        <>
-            <Container className={classes.myRoot} >
-                <Typography variant="h5">My Custom Accordian Style</Typography>
-
-                {
-                    question.map((item) => {
-                        return <MyAccordian key={item.id} {...item} />
-                    })
-                }
-            </Container>
-        </>
-    );
+        {question.map((item) => {
+          return <MyAccordian key={item.id} {...item} />;
+        })}
+      </Container>
+    </>
+  );
 }
 
 export default Accordian;
